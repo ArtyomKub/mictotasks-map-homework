13723ef984c212fd8e3de407bac7647d0ad7db5c
Artyom Kubyshkin <coder.kub.lover@gmail.com>
2024-05-16 14:26:24 +0200

    Import NewComponent in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {NewComponent} from './NewComponent';
 
 function App() {
-  return (
-    <div>
-        <NewComponent/>
-    </div>
-  );
+    const [topCars] = useState(
+        [
+            {manufacturer: 'BMW', model: 'm5cs'},
+            {manufacturer: 'Audi', model: 'a4b8'},
+            {manufacturer: 'Mercedec', model: 'S-class'},
+            {manufacturer: 'Volksvagen', model: 'Touareg'},
+        ]
+    )
+    return (
+        <div>
+                <NewComponent/>
+        </div>
+    );
 }
 
 export default App;
