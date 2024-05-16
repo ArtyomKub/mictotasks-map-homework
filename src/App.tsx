8e51da7d33d1f827303294b4f0b16d9ceca8b341
Artyom Kubyshkin <coder.kub.lover@gmail.com>
2024-05-16 14:34:55 +0200

    Complete

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
     )
     return (
         <div>
-                <NewComponent/>
+                <NewComponent topCars={topCars}/>
         </div>
     );
 }
--- src/NewComponent.tsx
+++ src/NewComponent.tsx
@@ -1,9 +1,29 @@
 import React from 'react';
 
-export const NewComponent = () => {
+type EliteCarsType = {
+    topCars: Array<TopCarsType>
+}
+
+type TopCarsType = {
+    manufacturer: string
+    model: string
+}
+
+export const NewComponent = (props: EliteCarsType) => {
+
     return (
         <div>
-
+            <ul>
+                {props.topCars.map((object: TopCarsType, index) => {
+                    return (
+                        <li key={index}>
+                            <span>{index+1}</span>
+                            <span>{object.manufacturer}</span>
+                            <span>{object.model}</span>
+                        </li>
+                    )
+                })}
+            </ul>
         </div>
     );
 };
