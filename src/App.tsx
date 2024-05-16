3e9c23da0f19bc91d760455201b68ab2503dd681
Artyom Kubyshkin <coder.kub.lover@gmail.com>
2024-05-16 13:56:27 +0200

    Import NewComponent in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import {NewComponent} from './NewComponent';
 
 function App() {
   return (
     <div>
-
+        <NewComponent/>
     </div>
   );
 }
