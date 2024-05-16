ad599c45b0d6530b5c052886ba32198cade6585a
Artyom Kubyshkin <coder.kub.lover@gmail.com>
2024-05-16 13:55:04 +0200

    Create a constanta NewComponent.tsx and add massive (topCars)

---
+++ src/NewComponent.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export const NewComponent = () => {
+
+    const topCars = [
+        {manufacturer: 'BMW', model: 'm5cs'},
+        {manufacturer: 'Audi', model: 'a4b8'},
+        {manufacturer: 'Mercedec', model: 'S-class'},
+        {manufacturer: 'Volksvagen', model: 'Touareg'},
+    ]
+    return (
+        <div>
+            newcomponent
+        </div>
+    );
+};
